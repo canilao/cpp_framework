849d25bc937ae9b47b6234f86f0450ac452339e8
canilao <canilao@55e5c601-11dd-bd4b-a960-93a593583956>
2010-09-14 20:06:46 +0000

    Adding synchronization file specific to windows.

---
+++ threads/winthreads/synchronization.h
@@ -0,0 +1,116 @@
+/******************************************************************************/
+//
+/*! \brief  Utility for synchronizing threads in windows applications, 
+            using CRITICAL_SECTION.  To use, create a mutex object like you
+            would when you create a critical section.  Then, when thread 
+            critical operations occur, use the lock object to enter the 
+            critical section.  This method lowers the number of lines of code 
+            that needs to be written to use CRITICAL_SECTION and to make 
+            synchronization simpler.
+
+            See below for an example:
+
+            // Here is some memory that needs to be thread safe. 
+            int Variable = 0;
+
+            // Some where in code, create a Mutex object that will exist for
+            // as long as it is needed.
+
+            Mutex MyVariableMutex;
+
+            ... Some where else in the code.
+
+            // Some thread critical operations are going to occur so lock our
+            // associated mutex.  Using brackets makes it easy to visualize
+            // when the lock starts and when it ends.
+
+            {
+               Lock lockObj(MyVariableMutex);
+
+               ++Variable;
+            }
+
+    \note   C Anilao    07/22/2010  Added header.
+
+*******************************************************************************/
+
+#pragma once
+
+#include <windows.h>
+
+/******************************************************************************/
+//
+/*! \class  Mutex
+
+    \brief  It is equivalent to the CRITICAL_SECTION object.  To use, just 
+            instantiate Mutex to aquire a CRITICAL_SECTION. 
+
+*******************************************************************************/
+class Mutex
+{
+   friend class Lock;
+
+public:
+
+   // Constructor.
+   Mutex () 
+   { 
+      InitializeCriticalSection (& _critSection); 
+   }
+
+   // Destructor.
+   ~Mutex () 
+   { 
+      DeleteCriticalSection (& _critSection); 
+   }
+
+private:
+
+   // This is called when the critical section is needed to be locked.
+   void Acquire ()
+   {
+      EnterCriticalSection (& _critSection);
+   }
+
+   // This is called when the critical section is needed to be released.
+   void Release ()
+   {
+      LeaveCriticalSection (& _critSection);
+   }
+
+   CRITICAL_SECTION _critSection;
+};
+
+/******************************************************************************/
+//
+/*! \class  Lock
+
+    \brief  This object is a mechanism for locking a CRITICAL_SECTION.  To use
+            just instantiate the object, with the corresponding Mutex in the 
+            as the parameter in the constructor.  This will automatically lock.
+            When the lock object is destroyed, then the CRITICAL_SECTION is 
+            released.
+
+*******************************************************************************/
+class Lock
+{
+public:
+
+   // Acquire the state of the semaphore
+   Lock ( Mutex & mutex )
+      : _mutex(mutex)
+   {
+      _mutex.Acquire();
+   }
+
+   // Release the state of the semaphore
+   ~Lock ()
+   {
+      _mutex.Release();
+   }
+
+private:
+
+   // Mutex passed in the constructor.
+   Mutex & _mutex;
+};
